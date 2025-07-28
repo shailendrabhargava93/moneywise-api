81d141230873faf427d27374a20ef6eca529828a
Shailendra Bhargava <shailendrabhargavaa@gmail.com>
2025-07-28 13:42:10 +0530

    modify response

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,22 +1,4 @@
-import { getAllLabelsByEmail, getMembers, createMembers } from "../services/memberService.js";
-
-export const fetchAllLabels = async (req, res) => {
-  try {
-    const { email } = req.params;
-
-    if (!email) {
-      return res.status(400).json({
-        error: 'Email parameter is required'
-      });
-    }
-
-    const labels = await getAllLabelsByEmail(email);
-    res.status(200).json(labels);
-  } catch (error) {
-    console.error('Error in fetchAllBudgets:', error);
-    res.status(500).json({ error: error.message });
-  }
-};
+import { getMembers, createMembers } from "../services/memberService.js";
 
 
 export const createMembersData = async (req, res) => {
--- services/memberService.js
+++ services/memberService.js
@@ -1,29 +1,5 @@
 import supabase from "../config/supabaseClient.js";
 
-export const getAllLabelsByEmail = async (email) => {
-  try {
-    const { data, error } = await supabase
-      .from("labels")
-      .select("labels")
-      .eq("user", email);
-
-    if (error) {
-      throw new Error(`Error fetching labels: ${error.message}`);
-    }
-
-    if (!data || data.length === 0) {
-      return [];
-    }
-
-    // Split the string by comma and flatten the array
-    return data.flatMap(item => item.labels.split('|'));
-  } catch (error) {
-    console.error("Error in getAllLabelsByEmail:", error);
-    throw error;
-  }
-};
-
-
 export const createMembers = async (membersData) => {
   try {
     const { data, error } = await supabase
@@ -51,15 +27,12 @@ export const getMembers = async (email) => {
       .eq("user", email);
 
     if (error) {
-      console.error('Supabase Query Error:', {
-        message: error.message,
-        details: error.details,
-        hint: error.hint
-      });
       throw new Error(`Error fetching members: ${error.message}`);
     }
-    console.log("Fetched members:", members);
-    return members;
+    return members.map(m => ({
+      name: m.memberName,
+      avatar: m.memberAvatar
+    }));
   } catch (error) {
     console.error("Error in getMembers:", error);
     throw error;
